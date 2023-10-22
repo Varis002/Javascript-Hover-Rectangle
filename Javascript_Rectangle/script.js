ff767dfebf18cd1259a799f1c6695e7385d19723
Varis Rajak <148039754+Varis002@users.noreply.github.com>
2023-10-22 14:07:00 +0530

    Update script.js 2

--- Javascript_Rectangle/script.js
+++ Javascript_Rectangle/script.js
@@ -2,10 +2,10 @@ const rectangle = document.querySelector("#center");
 
 rectangle.addEventListener("mousemove", (details) => {
     const rectanglelocation = rectangle.getBoundingClientRect();
-    let insiderectangleValue = parseInt(details.clientX - rectanglelocation.left) + 1
+    const insiderectangleValue = parseInt(details.clientX - rectanglelocation.left) + 1
     if(insiderectangleValue < rectanglelocation.width/2){
         console.log("left: " + (rectanglelocation.width/2-insiderectangleValue));
-        let redcolor = (gsap.utils.mapRange(0,rectanglelocation.width/2,1,0,insiderectangleValue))
+        const redcolor = (gsap.utils.mapRange(0,rectanglelocation.width/2,1,0,insiderectangleValue))
         const titleColor = gsap.utils.mapRange(0,1,0,255,redcolor)
         rectangle.innerHTML = "Red Color Opacity: " + redcolor.toFixed(2)
         rectangle.style.color=`rgb(${titleColor},${titleColor},${titleColor})`
@@ -17,7 +17,7 @@ rectangle.addEventListener("mousemove", (details) => {
     }
     else{
         console.log("right: " + (insiderectangleValue-rectanglelocation.width/2));
-        let blueColor = (gsap.utils.mapRange(rectanglelocation.width/2,rectanglelocation.width,0,1,insiderectangleValue))
+        const blueColor = (gsap.utils.mapRange(rectanglelocation.width/2,rectanglelocation.width,0,1,insiderectangleValue))
         console.log(blueColor);
         gsap.to(rectangle,{
             backgroundColor: `rgba(0,0,255,${blueColor})`,
