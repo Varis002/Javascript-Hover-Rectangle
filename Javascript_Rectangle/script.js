57d33d999d9c28ee04f54f5ec9c728d4c9859f40
Varis Rajak <148039754+Varis002@users.noreply.github.com>
2023-10-22 14:06:06 +0530

    Update script.js

--- Javascript_Rectangle/script.js
+++ Javascript_Rectangle/script.js
@@ -1,12 +1,12 @@
-let rectangle = document.querySelector("#center");
+const rectangle = document.querySelector("#center");
 
 rectangle.addEventListener("mousemove", (details) => {
-    let rectanglelocation = rectangle.getBoundingClientRect();
-    var insiderectangleValue = parseInt(details.clientX - rectanglelocation.left) + 1
+    const rectanglelocation = rectangle.getBoundingClientRect();
+    let insiderectangleValue = parseInt(details.clientX - rectanglelocation.left) + 1
     if(insiderectangleValue < rectanglelocation.width/2){
         console.log("left: " + (rectanglelocation.width/2-insiderectangleValue));
-        var redcolor = (gsap.utils.mapRange(0,rectanglelocation.width/2,1,0,insiderectangleValue))
-        let titleColor = gsap.utils.mapRange(0,1,0,255,redcolor)
+        let redcolor = (gsap.utils.mapRange(0,rectanglelocation.width/2,1,0,insiderectangleValue))
+        const titleColor = gsap.utils.mapRange(0,1,0,255,redcolor)
         rectangle.innerHTML = "Red Color Opacity: " + redcolor.toFixed(2)
         rectangle.style.color=`rgb(${titleColor},${titleColor},${titleColor})`
         console.log(redcolor);
@@ -17,13 +17,13 @@ rectangle.addEventListener("mousemove", (details) => {
     }
     else{
         console.log("right: " + (insiderectangleValue-rectanglelocation.width/2));
-        var blueColor = (gsap.utils.mapRange(rectanglelocation.width/2,rectanglelocation.width,0,1,insiderectangleValue))
+        let blueColor = (gsap.utils.mapRange(rectanglelocation.width/2,rectanglelocation.width,0,1,insiderectangleValue))
         console.log(blueColor);
         gsap.to(rectangle,{
             backgroundColor: `rgba(0,0,255,${blueColor})`,
             ease: Power4
         })
-        let titleColor = gsap.utils.mapRange(0,1,0,255,blueColor)
+        const titleColor = gsap.utils.mapRange(0,1,0,255,blueColor)
         rectangle.innerHTML = "Blue Color Opacity: " + blueColor.toFixed(2)
         rectangle.style.color=`rgb(${titleColor},${titleColor},${titleColor})`
 
